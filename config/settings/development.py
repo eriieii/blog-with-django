5c116a6b88d2be9cfc633c759c62d5c14ba3ebc3
eriieii <eriiferdian92@gmail.com>
2022-11-27 00:14:59 +0700

    Config + Social Auth

---
+++ config/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+#Database Config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+#Overrider Development
+INSTALLED_APPS += [
+    'blog.apps.BlogConfig'
+]
